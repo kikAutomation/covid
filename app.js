ff6fbc9e8a606dd39d6d45fe84fbfdd23451a6ac
acsia <ashoukat@visteon.com>
2020-03-27 18:15:33 +0530

    modal cart

--- app.js
+++ app.js
@@ -17,19 +17,55 @@
 
 const express = require('express');
 const app = express();
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+require("firebase/database");
+
+var fs = require('fs');
+
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAkGopYf5Rgen_GgBpJ_VwQAzNZ1LRx3sg",
+    authDomain: "covid-19-help-desk.firebaseapp.com",
+    databaseURL: "https://covid-19-help-desk.firebaseio.com",
+    projectId: "covid-19-help-desk",
+    storageBucket: "covid-19-help-desk.appspot.com",
+    messagingSenderId: "259588761043",
+    appId: "1:259588761043:web:b45b72ba9d8707bb6dfb26",
+    measurementId: "G-KZ7SN1WW8F"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
 var path = require('path');
 
 //app.set('view engine', 'pug');
-
+app.use(express.urlencoded());
 // Use the built-in express middleware for serving static files from './public'
 app.use(express.static('public'));
 app.get('/volunteer_login', (req, res) => {
- // res.render('hm.pug');
+ 
 res.sendFile(path.join(__dirname + '/volunteer_login.html'));
 });
+app.post('/to_cart', (req, res) => {
+if(req.body.phone!=""){
+// res.sendFile(path.join(__dirname + '/cart.html'));
 
+fs.readFile(path.join(__dirname + '/shopping_cart.html'), function(err, data) {
+    res.write(data);
+   // res.write("<h1>ttttttttttt</h1>");
+    return res.end();
+  });
+
+}else{
+res.status(404).send('Unautharised').end();
+}
+});
 app.get('/', (req, res) => {
- // res.render('hm.pug');
+
  res.sendFile(path.join(__dirname + '/hm.html'));
 });
 
---
+++ hm.js
@@ -0,0 +1,71 @@
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+}
+
+function toggleCard(cardElementName) {
+    var volunteerLoginCard = document.getElementById("Volunteer_Login_Card");
+    var trackRequestCard = document.getElementById("Track_Order_Card");
+    var medrepLoginCard = document.getElementById("Medicalrep_Login_Card");
+
+    switch (cardElementName) {
+        case "Volunteer_Login_Card":
+            trackRequestCard.style.display = "none";
+            medrepLoginCard.style.display = "none";
+            volunteerLoginCard.style.display = "block";
+            break;
+        case "Track_Order_Card":
+            volunteerLoginCard.style.display = "none";
+            medrepLoginCard.style.display = "none";
+            trackRequestCard.style.display = "block";
+            break;
+        case "Medicalrep_Login_Card":
+            trackRequestCard.style.display = "none";
+            volunteerLoginCard.style.display = "none";
+            medrepLoginCard.style.display = "block";
+            break;
+    }
+}
+
+function help() {
+    var x = document.getElementById("home");
+}
+
+$(document).ready(function () {
+    $("#post").click(function () {
+        document.getElementById("loading_ripple").style.visibility = "visible"
+        $("#home").fadeOut(1000);
+    });
+    $("#track").click(function () {
+        document.getElementById("loading_ripple").style.visibility = "visible"
+        $("#home").fadeOut(1000);
+    });
+    $("#login").click(function () {
+        document.getElementById("loading_ripple").style.visibility = "visible"
+        $("#home").fadeOut(1000);
+    });
+});
